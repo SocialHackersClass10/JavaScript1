0c767375ec5606a715f2f699347cab24eb71a207
manwlispro@gmail.com <manwlispro@gmail.com>
2020-01-15 11:25:57 +0200

    finished project for homework week1

---
+++ Week1/homework/js-exercises/logString.js
@@ -0,0 +1,7 @@
+'use strict'
+ let myString = "Manolis Protopsaltis"
+ console.log("my name");
+ console.log(myString);
+ myString ="kali xronia";
+ console.log("kali xronia");
+ console.log(myString);
