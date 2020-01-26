776389eb420ee869964666a229170140cf38dfd5
Jallow9 <56887061+Jallow9@users.noreply.github.com>
2020-01-26 18:06:36 +0200

    sliceChange
    some comments removed

--- Week2/homework/js-exercises/removeCommas.js
+++ Week2/homework/js-exercises/removeCommas.js
@@ -6,18 +6,3 @@ console.log(commars.length);
 let removeCommars = commars.split(',').join(""); //replace(/,/g, ''); 
 console.log(removeCommars);    
 
-
-
-/*for(let i = 0; i<=20; i++);{
-    if (i === 0);{
-        console.log(i +  ' is even');
-        }
-        else if (1 % 2 === 0);{
-            console.log(i + ' is even');
-        }
-        else {
-            console.log(i + ' is odd');
-        }
-}
-
-*/
