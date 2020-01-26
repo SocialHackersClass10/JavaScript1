8eeb6e026e5ccfa2745b3242115c75f420177e41
Jallow9 <56887061+Jallow9@users.noreply.github.com>
2020-01-26 18:49:54 +0200

    Delete removeCommas.js
    delete removeCommas.js

--- Week2/homework/js-exercises/removeCommas.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-let commars = "hello,this,is,a,difficult,to,read,sentence";
-console.log(commars.length);
-
-let removeCommars = commars.split(',').join(""); //replace(/,/g, ''); 
-console.log(removeCommars);    
-
