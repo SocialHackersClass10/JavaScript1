44b49eb1d525eba18117ff57f79463102bb7620a
mouzakiskir <matsakonistisbm@hotmail.com>
2020-02-03 02:15:21 +0200

    made changes

--- Week1/homework/js-exercises/6_arrayAnimals.js
+++ Week1/homework/js-exercises/6_arrayAnimals.js
@@ -1,25 +1,23 @@
 'use strict'
 /*Declare variable and assign to it an empty array. 
 Make sure that the name of the variable indicates it contains more than 1 item. For example items instead of item.*/
-let animals=["tiger","lion","panther"];
+let array1=new Array;
 
 /*Write a console.log statement that explains in words what you think the value of the array is.*/
-console.log("Arrays are used to store multiple values or strings in a single valiable")
+console.log("The value of the array is empty")
 
 /*Write a console.log statement that logs the array.*/
-console.log(animals);
+console.log(array1);
 
 /*Create a new variable with an array that has 3 of your favorite animals, each in a different string. 
 Make sure the name of the variables says something about what the variable contains.*/
-let tiger=console.log(animals[0]);
-let lion=console.log(animals[1]);
-let panther=console.log(animals[2]);
+let animals=["lion","tiger","panther"];
 
 /*Write a console.log statement that logs the second array.*/
-console.log(animals[1]);
+console.log(animals);
 
 /*Add a statement that adds another string ("Piglet)" to the array of animals.*/
 animals.push("Piglet");
 
 /*Write a console.log statement that logs the second array!*/
-console.log(animals[1]);
+console.log(animals);
