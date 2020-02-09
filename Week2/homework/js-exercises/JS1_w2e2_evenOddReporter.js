d24ddcfcfcbcc1338364887abf19659bda5b7d58
GMouzakis <skerpanistis@gmail.com>
2020-02-09 23:58:24 +0200

    JavaScript1 Week2 exercises

---
+++ Week2/homework/js-exercises/JS1_w2e2_evenOddReporter.js
@@ -0,0 +1,14 @@
+'use strict'
+
+for(let i = 0; i<=20; i++){ //1
+	if(i%2==0) //2
+		console.log(i + " is even"); //3
+	else console.log(i + " is odd"); //4
+}
+
+/*
+1. Create a `for` loop, that iterates from 0 to 20.
+2. Create a conditional statement that checks if the value of the counter variable is odd or even.
+3. If it's odd, log to the console `The number [PUT_NUMBER_HERE] is odd!`.
+4. If it's even, log to the console `The number [PUT_NUMBER_HERE] is even!`.
+*/
