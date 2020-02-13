ca68def1959819cd9f96401b3f39c40616cf25fb
GMouzakis <skerpanistis@gmail.com>
2020-02-13 16:24:12 +0200

    JS1 w2 project gradeCalculator fix

--- Week2/homework/Grade Calculator/JS1_w2_project_gradeCalculator.js
+++ Week2/homework/Grade Calculator/JS1_w2_project_gradeCalculator.js
@@ -7,14 +7,14 @@ function gradeCalculator(score){ // function definition
     else if(score>=50 && score<=59) grade = 'E';
     else if(score>=60 && score<=69) grade = 'D';
     else if(score>=70 && score<=79) grade = 'C';
-    else if(score>=80 && score<=59) grade = 'B';
+    else if(score>=80 && score<=89) grade = 'B';
     else if(score>=90 && score<=100) grade = 'A'; //*2
    
     if(score>=0 && score<=100) console.log("You got a "+grade+" ("+score+"%)!"); //1 , 3 We print the results to console and score to percentage convertion
     else console.log("Wrong input, your score must be between 0 and 100");
 }
 
-gradeCalculator(67);
+gradeCalculator(Math.floor(Math.random()*100));
 
 /*
 1. convert the score into a percentage
