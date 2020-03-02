b3c5465483f1e3ed07faa48e11041ebf53999a96
ligopsychakis-michalis <ligopsyxakis.m@gmail.com>
2020-03-02 21:56:13 +0200

    change all js of calculator

--- Week1/HTML_Calculator/calc.js
+++ Week1/HTML_Calculator/calc.js
@@ -1,110 +1,59 @@
-let screen = document.getElementById("screen");
-let C = document.getElementById("C");
-let back = document.getElementById("back");
-let devide = document.getElementById("devide");
-let seven = document.getElementById("seven");
-let eight = document.getElementById("eight");
-let nine = document.getElementById("nine");
-let multi = document.getElementById("multi");
-let four = document.getElementById("four");
-let five = document.getElementById("five");
-let six = document.getElementById("six");
-let minus = document.getElementById("minus");
-let one = document.getElementById("one");
-let two = document.getElementById("two");
-let three = document.getElementById("three");
-let plus = document.getElementById("plus");
-let zero = document.getElementById("zero");
-let dec = document.getElementById("dec");
-let equal = document.getElementById("equal");
+const buttons = document.querySelector(".keyboard");
+const screen = document.querySelector(".screen");
+let arr = [];
+let symb = true;
 
-function playSound() {
-    var sound = document.getElementById("audio");
-    sound.play();
+//event: click button
+buttons.addEventListener("click", e => {
+  if(e.target.classList.contains("num")){
+    if(screen.innerText != "/" && screen.innerText != "X" && screen.innerText != "+" && screen.innerText != "-"){
+      screen.innerText += e.target.innerText;
+    }else{ 
+      screen.innerText = e.target.innerText;
+    }
+    symb = false;
+  };  
+  if(e.target.innerText == "C"){
+    screen.innerText = "";
+    arr = [];
+    symb = true;
+  };
+  if(e.target.innerText == "<="){
+    screen.innerText = screen.innerText.slice(0,screen.innerText.length-1);
+  };
+  if(e.target.classList.contains("symbol")){
+    if(symb == false){
+      arr.push(screen.innerText);
+      computation();
+      arr.push(e.target.innerText);
+      screen.innerText = e.target.innerText;
+      symb = true;
+    };  
+  };
+  if(e.target.id == "equal"){
+    arr.push(screen.innerText);
+    computation();
+    screen.innerText = arr[0];
+    arr = [];
+  };
+});   
+
+
+
+function computation(){
+  if (arr.length == 3){
+    if(arr[1] == "+"){
+      arr[0] = parseFloat(arr[0]) + parseFloat(arr[2]);
+    }else if(arr[1] == "-") {
+      arr[0] = parseFloat(arr[0]) - parseFloat(arr[2]);
+    }else if(arr[1] == "X") {
+      arr[0] = parseFloat(arr[0]) * parseFloat(arr[2]);
+    }else if(arr[1] == "/") {
+      arr[0] = parseFloat(arr[0]) / parseFloat(arr[2]);
+    }; 
+    arr.pop();
+    arr.pop();
+    symb = false;
+    return;
+  }  
 }
-
-let choose = [C,back,devide,seven,eight,nine,multi,four,five,
-    six,minus,one,two,three,plus,zero,dec,equal]
-
-let result = 0;
-let value2 = 0;
-let operator = "";
-let resultShowed = false;
-
-for (let i = 0; i < choose.length; i++){
-    function playSound(){
-        var sound = document.getElementById("audio");
-        sound.play();
-    }    
-    choose[i].onclick = function(){
-        if (screen.innerHTML === "0"){
-            screen.innerHTML = "";
-        }   
-        if (choose[i] == C){
-            screen.innerHTML = "0";
-            result = 0;
-            value2 = 0; 
-        // }else if (choose[i] == back) {
-        //     screen.innerHTML = screen.innerHTML.slice(0,screen.innerHTML.length-1);
-        //     if (screen.innerHTML.length == 0){
-        //         screen.innerHTML = "0";
-        //     }
-        }else if (choose[i] == dec && screen.innerHTML.indexOf(".") > -1){
-            //there is already a decimal symbol
-        }else if (choose[i] == plus){
-            result += parseFloat(screen.innerHTML);
-            screen.innerHTML = "+";
-            operator = plus.innerHTML;
-        }else if (choose[i] == minus){
-            result = parseFloat(screen.innerHTML);
-            screen.innerHTML = "-";
-            operator = minus.innerHTML;
-        }else if (choose[i] == multi){
-            result = parseFloat(screen.innerHTML);
-            screen.innerHTML = "x";
-            operator = multi.innerHTML;
-        }else if (choose[i] == devide){
-            result = parseFloat(screen.innerHTML);
-            screen.innerHTML = "/";
-            operator = devide.innerHTML;
-        }else if (choose[i] == equal){
-            value2 = parseFloat(screen.innerHTML)
-            if (isNaN(result)){
-                result = 0;
-            }
-            if (isNaN(value2)){
-                value2 = 0;
-            }
-            if (operator == plus.innerHTML){
-                result += value2;
-            }else if(operator == minus.innerHTML){
-                result -= value2;
-            }else if(operator == multi.innerHTML){
-                result *= value2;
-            }else if(operator == devide.innerHTML){
-                result /= value2;
-            }else{
-                result = value2;
-            }
-            screen.innerHTML = result.toString();
-            result = 0;
-            value2 = 0;
-            operator = "";
-            resultShowed = true;   
-        }else if (choose[i] == back){
-            screen.innerHTML = screen.innerHTML.slice(0,screen.innerHTML.length-1);
-            if (screen.innerHTML.length == 0 || resultShowed === true){
-                screen.innerHTML = "0";
-            }
-        }else if (screen.innerHTML == plus.innerHTML || screen.innerHTML == minus.innerHTML
-            || screen.innerHTML == devide.innerHTML || screen.innerHTML == multi.innerHTML){
-            screen.innerHTML = choose[i].innerHTML;
-            resultShowed = false;
-        }else if (resultShowed === true){
-            screen.innerHTML = choose[i].innerHTML;
-            resultShowed = false;    
-        }else{
-            screen.innerHTML += choose[i].innerHTML;
-        }
-    }           
-};
--- Week2/js-exercises/recipe-card.js
+++ Week2/js-exercises/recipe-card.js
@@ -6,10 +6,13 @@ let myMealRecipe = {
 }
 for (let key in myMealRecipe){
     if (myMealRecipe[key] == myMealRecipe.title){ 
-        console.log("Meal name: " + myMealRecipe[key])
+        console.log("Meal Name: " + myMealRecipe[key])
     }else if (myMealRecipe[key] == myMealRecipe.servings){
         console.log("Serves: " + myMealRecipe[key])
     }else {
-        console.log("Ingredients: " + myMealRecipe[key])
+        console.log("Ingredients:");
+        for (let i = 0; i < myMealRecipe.ingredients.length; i++){
+            console.log(myMealRecipe[key][i]);
+        }
     }
 }
